ace9e25279bdf8745212ad4b9d8432a644658f9d
ya790206 <ya790206@gmail.com>
2014-03-05 23:29:49 +0800

    Add docker for displaying output. And add indent for edit.

--- edit.py
+++ edit.py
@@ -1,5 +1,4 @@
 import sys
-from core import catch_local_vars
 from PySide import QtGui, QtCore
 
 
@@ -8,6 +7,7 @@ class Edit(QtGui.QTextEdit):
     def __init__(self):
         super(Edit, self).__init__()
         self.initUI()
+        self.key_enter_callback_list = []
 
     def initUI(self):
         self.show()
@@ -15,12 +15,31 @@ class Edit(QtGui.QTextEdit):
 
     def keyPressEvent(self, event):
         if event.key() in [QtCore.Qt.Key_Enter, QtCore.Qt.Key_Return]:
-            line_numbere = self.textCursor().blockNumber()
-            code = self.toPlainText()
-            env, local_vars, err = catch_local_vars(code, stop=line_numbere)
-            print local_vars
-            print err
+            for callback in self.key_enter_callback_list:
+                callback()
+            self.indent()
+            return True
+
+
         return super(Edit, self).keyPressEvent(event)
+
+    def add_ener_callback(self, callback):
+        self.key_enter_callback_list.append(callback)
+
+    def keyReleaseEvent(self, event):
+
+        return super(Edit, self).keyReleaseEvent(event)
+
+    def indent(self):
+        text = self.toPlainText()
+        lines = text.split('\n')
+        if len(lines) < 1:
+            return
+        last_line = lines[-1]
+        start_space_count = len(last_line) - len(last_line.lstrip())
+        if len(last_line) > 0 and last_line[-1] == ':':
+            start_space_count += 4
+        self.append(' ' * start_space_count)
 
 
 def main():
--- main.py
+++ main.py
@@ -1,12 +1,54 @@
 import sys
 from PySide import QtGui, QtCore
 from edit import Edit
+from core import catch_local_vars
+
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.main_editor = Edit()
+        self.main_editor.add_ener_callback(self.catch_local_vars)
+        self.setCentralWidget(self.main_editor)
+        self.createDockWindows()
+
+    def createDockWindows(self):
+        self.test_code_docker = QtGui.QDockWidget("Test Code", self)
+        self.test_code_editor = QtGui.QTextEdit(self.test_code_docker)
+        self.test_code_docker.setWidget(self.test_code_editor)
+        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.test_code_docker)
+
+        self.local_vars_docker = QtGui.QDockWidget("Local vars", self)
+        self.local_vars_view = QtGui.QTextEdit(self.local_vars_docker)
+        self.local_vars_view.setReadOnly(True)
+        self.local_vars_docker.setWidget(self.local_vars_view)
+        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.local_vars_docker)
+
+        self.error_docker = QtGui.QDockWidget("error", self)
+        self.error_view = QtGui.QTextEdit(self.error_docker)
+        self.error_view.setReadOnly(True)
+        self.error_docker.setWidget(self.error_view)
+        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.error_docker)
+
+    def catch_local_vars(self):
+        line_numbere = self.main_editor.textCursor().blockNumber()
+        code = self.main_editor.toPlainText()
+        env, local_vars, err = catch_local_vars(code, stop=line_numbere)
+        if err:
+            self.error_view.setText(str(err))
+        else:
+            code = self.test_code_editor.toPlainText()
+            env, local_vars, err = catch_local_vars(code, env=env)
+            self.error_view.setText(str(err))
+            self.local_vars_view.setText(str(local_vars))
 
 
 def main():
 
     app = QtGui.QApplication(sys.argv)
-    ex = Edit()
+    ex = MainWindow()
+    ex.show()
     sys.exit(app.exec_())
 
 
